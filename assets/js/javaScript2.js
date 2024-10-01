3c4d13154ed343363ae4acb57d3bc80e2c5f40e8
MC Foster <mariechantalfoster@gmail.com>
2024-10-01 14:05:46 -0400

    added banner gruesome goonz

--- assets/js/javaScript2.js
+++ assets/js/javaScript2.js
@@ -4,21 +4,12 @@ const imageContainer = document.querySelector('.img-container');
 imageContainer.addEventListener('click', function (event) {
   const element = event.target;
 
-
   if (element.matches('img')) {
-
     const state = element.getAttribute('data-state');
-
     if (state === 'still') {
-      // Change the data-state attribute's value
-      // There are two different ways this attribute can be set
-      // element.dataset.state = 'animate';
       element.setAttribute('data-state', 'animate');
-
-      // Update the image's source to the string being stored in the data-animate attribute
       element.setAttribute('src', element.dataset.animate);
     } else {
-      // Change the attributes back to their non-animated values
       element.dataset.state = 'still';
       element.setAttribute('src', element.dataset.still);
     }
