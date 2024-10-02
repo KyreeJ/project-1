d38220ead0ff30bdf97a03fe63ceb57957cbd77d
Brandon <brandonlanes@gmail.com>
2024-10-02 17:00:42 -0400

    updates to index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,22 @@
 window.onload = function() {
     const gif = document.getElementById('jumpscare-gif');
     const staticImage = document.getElementById('jumpscare-image');
-    // const sound = document.getElementById('jumpscare-sound');
+    const sound = document.getElementById('jumpscare-sound');
 
     // function showJumpscare() {
         // gif.style.display = 'block';
         // staticImage.style.display = 'none';
         // sound.onplay();
     
-    setTimeout(() => {
-        gif.style.display = 'none';
-        staticImage.style.display = 'block';
-            // setTimeout(() => {
-            //     showJumpscare();
-            // }, 2000);
-    }, 4000);
+    setInterval(() => {
+        gif.style.display = 'block';
+        staticImage.style.display = 'none';
+            setTimeout(() => {
+                gif.style.display = 'none';
+                staticImage.style.display = 'block';
+                sound.play();
+            }, 6000);
+    }, 9000);
 };
 
 // const options = ['Halloween', 'Friday the 13th', 'Texas Chainsaw Massacre', 'Scream', 'The Shining',
